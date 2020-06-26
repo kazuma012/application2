e867e3bd14e2eaa73bb78f8d7567ff27123af33c
大竹 <ootakeakira@ootakenoiMac.local>
2020-06-26 21:13:01 +0900

    ouyoukadai3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 20, minimum: 2 }
   validates :introduction, length: { maximum: 50 }
-  has_many :books, dependent: :destroy
   attachment :profile_image
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
